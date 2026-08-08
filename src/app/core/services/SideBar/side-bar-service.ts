ceb27a8759131bd616570b7e733f757ae730ea49
Chiranjib Chakraborty <chiranjib@akijbashir.com>
2026-08-08 10:56:13 +0600

    api url bug solved

--- src/app/core/services/SideBar/side-bar-service.ts
+++ src/app/core/services/SideBar/side-bar-service.ts
@@ -3,20 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { GlobalConstant } from 'src/app/core/constants/global.constants';
 import { ISideBarMenu } from 'src/app/core/model/SideBar/SideBar';
-import { environment } from 'src/environments/environment.prod';
-
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SideBarService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   GetSideBarData(enroll: number): Observable<ISideBarMenu[]> {
     return this.http.get<ISideBarMenu[]>(
-      `${environment.API_URL}${GlobalConstant.API_END_POINTS.SideBar}/${enroll}`
+      `${environment.API_URL}${GlobalConstant.API_END_POINTS.SideBar}/${enroll}`,
     );
   }
 }
-
